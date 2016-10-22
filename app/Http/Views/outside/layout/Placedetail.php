65901b24f280e77e59ff8151a9d6f67fda716ecf
nguyenx2quoc <nguyenx2quoc@gmail.com>
2016-10-22 15:15:12 +0700

    Add new files by: nguyenx2quoc

---
+++ app/Http/Views/outside/layout/Placedetail.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../../../../../public/assets/outside/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../../../../../public/assets/outside/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="quoccss.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body style="width: 960px; margin: auto">
+        <div style="width: 960px;height: 210px" class="row-formanh">
+            <div class="col-sm-3">
+                <img src="download.jpg" width="240" height="200" alt="download"/>
+            </div>
+            <div class="col-sm-3">
+                <img src="download.jpg" width="240" height="200" alt="download"/>
+            </div>
+            <div class="col-sm-3">
+                <img src="download.jpg" width="240" height="200" alt="download"/>
+            </div>
+            <div class="col-sm-3">
+                <img src="download.jpg" width="240" height="200" alt="download"/>
+            </div>
+        </div>
+        <div class="contents" style="margin: auto; width: 960;height: 128">
+            <div class="row" style="margin: auto">
+                <div>
+                    <div class="col-sm-2">
+                        <img src="download.jpg" width="100" height="100" alt="download"/>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="row">
+                            <h2>Sulbing Q Dessert</h2>
+                        </div>
+                        <div>
+                            <span>Đây là ở đâu, ở đâu là ở đây</span>
+                        </div>
+                        <div>
+                            <span>TP.Hồ Chí Minh chứ đâu</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-3" style="margin-top: 60px">
+                        <div class="col-xs-6">
+                            <button class="btn-primary btn-sm">Save</button>
+                        </div>
+                        <div class="col-xs-6">
+                            <button class="btn-primary btn-sm">Share</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin: auto; width: 960;height: 128">
+                <div class="col-sm-8">
+                    <div class="navbar navbar-default" style="margin: auto; width: 960;height: 128">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                              <a class="navbar-brand" href="#">Tips</a>
+                              <a class="navbar-brand" href="#">Photos</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <button class="btn-primary">YêuThich</button>
+                    <button class="btn-primary">BinhThuong</button>
+                    <button class="btn-primary">KhongThich</button>
+                </div>
+            </div>
+            
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-sm-8" style="padding-left: 30px;">
+                    <div class="form-group" style="padding-left: 30px;">
+                        <div class="row"></div>
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <img src="download.jpg" width="30" height="30" alt="download"/>                                
+                            </div>
+                            <div class="col-sm-6">
+                                <textarea style="width: 448px;height: 73px" title="Bạn có ý tưởng gì" placeholder="Bạn có ý tưởng gì?"></textarea>
+                            </div>
+                        </div>
+                        <div class="row"></div>
+                    </div>
+                    <br>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading"></div>
+                        <div class="panel-body">Panel Content</div>
+                    </div>
+                    <div></div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="row">
+                        <img src="download.jpg" width="330" height="155" alt="download"/>
+                        <div class="list-group">
+                            <div class="list-group-item">
+                                <h4>
+                                <img src="download.jpg" width="16" height="16" alt="download"/>
+                                Sulbing Q Dessert </h4>                          
+                                <div style="margin-left: 50px;" >
+                                    <div>244 Bui Vien Street, Pham Ngu Lao </div>
+                                    <div>Ward, Dist 1, Hcmc</div>
+                                    <div>Thành phố Hồ Chí Minh</div>
+                                    <div>Thành phố Hồ Chí Minh</div>
+                                    <div>Việt Nam</div>
+                                    <button class="btn-link">Get directions</button>
+                                </div>
+                            </div>
+                            <div class="list-group-item">
+                                <div class="row" style="margin-left: 5px;color: green">
+                                    <img src="download.jpg" width="16" height="16" alt="download"/>
+                                    Open until Midnight
+                                    <button class="btn-link">(Show more)</button>                                    
+                                </div>
+                            </div>
+                            <div class="list-group-item">
+                                <div class="row" style="margin-left: 5px">
+                                    <img src="download.jpg" width="16" height="16" alt="download"/>
+                                    +84 8 3837 6707
+                                </div>
+                            </div>
+                            <div class="list-group-item">
+                                <div class="row" style="margin-left: 5px;color: green">
+                                    <img src="download.jpg" width="16" height="16" alt="download"/>
+                                     <button class="btn-link">sulbingq</button>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
