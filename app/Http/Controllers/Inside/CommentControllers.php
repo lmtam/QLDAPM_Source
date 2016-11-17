4eb6be611c8399bd778966b378b84c80a094e725
AnhTai1995 <anhtai.hcmus95@gmail.com>
2016-11-17 20:03:39 +0700

    Cập nhật css

---
+++ app/Http/Controllers/Inside/CommentControllers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Inside;
+
+
+use App\Http\Requests\Request;
+use App\MyCore\Inside\Routing\MyController;
+use View;
+
+
+
+
+
+class CommentControllers extends MyController{
+
+    private $_model = null;
+    private $_params = array();
+    public function __construct() {
+        $options = array();
+        $this->_params = \Request::all();
+        $this->data['params'] = $this->_params;
+        parent::__construct($options);
+
+
+        $this->data['controllerName'] = 'places';
+        $this->_model = new DistrictModels();
+
+    }
+
+
+
+
+}
